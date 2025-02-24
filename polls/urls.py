d28b0a5c33ad84f45b68bb1f0be03e0640f8011e
Peter Muhumuki <muhumukip@gmail.com>
2025-02-24 05:10:23 +0300

    Add ChoiceList and CreateVote API views; update URL routing

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 # from .views import PollsList, PollDetail
-from .apiviews import PollList, PollDetail
+from .apiviews import PollList, PollDetail, ChoiceList, CreateVote
 
 urlpatterns = [
     path('', PollList.as_view(), name='polls_list'),
-    path('<int:pk>/', PollDetail.as_view(), name='polls_detail')
+    path('<int:pk>/', PollDetail.as_view(), name='polls_detail'),
+    path('choices/', ChoiceList.as_view(), name='choice_list'),
+    path('vote/', CreateVote.as_view(), name='create_vote'),
 ]
